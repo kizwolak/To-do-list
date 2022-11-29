719ecbc257317e4928260c6080e912a892f4fb84
Krzysztof <ki.zwolak@gmail.com>
2022-11-29 05:50:57 +0100

    timeSorter creates arrays well, more work needed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const datefns = require("date-fns");
 import createProject from "./projectCreate.js";
+import timeSorter from "./timeSorter.js";
 
-dates = [];
+const dates = [];
 
 const testProject = createProject("test");
 
 testProject.addNew("test1", "desc", new Date(2022, 5, 28), "high");
 testProject.addNew("test2", "desc", new Date(2022, 5, 28), "high");
 
-console.log(testProject);
+timeSorter(testProject);
--- src/timeSorter.js
+++ src/timeSorter.js
@@ -1,16 +1,30 @@
 export default function timeSorter(object) {
+    const dates = [];
     object.arrayOfToDos.forEach(element => {
-        if (!dates.include(element.date)) {
+        if (!dates.includes(element.date)) {
+            const dateArray = [];
+            // create a date array that will hold objects related to that date
+            dates.push(dateArray);
+            // to display dates and priority dots - no description
             const dateToDo = {
                 date: element.date,
                 priority: element.priority,
-            }
-            dates.push(dateToDO);
-        } else if (arrayOfToDos.includes(element.date)) {
-
+            };
+            dateArray.push(dateToDo);
+            console.log(dateArray);
+        } else if (dates.includes(element.date)) {
+            const dateToDo = {
+                date: element.date,
+                priority: element.priority,
+            };
+            dateArray.push(dateToDo);
+            console.log(dateArray);
         }
     });
-}
+    return dates;
+}; 
+
+// This function generates only one array - fix!!
 
 // generacja nowego obiektu/array z datami? wtedy możnaby dodawać toDoObjects do daty, data byłaby generowana codziennie wraz z arrayem, do którego można wciskać wydarzenia
 
