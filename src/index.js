9460b71ad35e31fc8fcec213aeaaddff2bde945d
Krzysztof <ki.zwolak@gmail.com>
2022-11-30 18:06:07 +0100

    Begin HTML and CSS work

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 const datefns = require("date-fns");
 import createProject from "./projectCreate.js";
-import timeSorter from "./timeSorter.js";
-
-
-const testProject = createProject("test");
-
-
-testProject.addNew("test1", "desc", new Date(2022, 4, 30), "high");
-testProject.addNew("test2", "desc", new Date(2022, 5, 28), "high");
-testProject.addNew("test3", "desc", new Date(2022, 5, 28), "high");
+import './style.css'
 
 
 
-timeSorter(testProject);
+
+
 
--- src/timeSorter.js
+++ src/timeSorter.js
@@ -1,7 +1,7 @@
 export default function timeSorter(object) {
 
     const dates = [{name: "Tue Jun 28 2022 00:00:00 GMT+0200 (Central European Summer Time)", array: []}];
-
+    
     object.arrayOfToDos.forEach(element => {
         if (!(dates.some(dateArray => dateArray.name == element.date))) {
             console.log("if");
@@ -30,85 +30,68 @@ export default function timeSorter(object) {
             console.log("datesIndex =  " + datesIndex);
             let datesReference = dates[datesIndex];
             console.log(datesReference);
-            dates[datesIndex].array.push(dateToDo);
+            datesReference.array.push(dateToDo);
         }
     });
     console.log(dates);
     return dates;
 }
-    // const dates = [''];
 
-    // object.arrayOfToDos.forEach(element => {
-    //     dates.forEach(date => {
-    //         if (!(date.name == element.date)) {
-    //             let newArrayName = element.date;
-    //             class dateArray {
-    //                 constructor(name) {
-    //                     this.name = name;
-    //                     this.array = [];
-    //                 }
-    //             };
-    //             const newDate = new dateArray(newArrayName);
-    //             const dateToDo = {
-    //                 date: element.date,
-    //                 priority: element.priority,
-    //             };
-    //             newDate.array.push(dateToDo);
-    //             dates.push(newDate);
-    //         }
-    //     })
-    // }) 
-    // console.log(dates);
-    // return dates;
+// The below is code by Biff#4541 on The Odin Project's Discord channel. This code is a future reference.
+// This code will be used to group toDos by dates so that they can be displayed in a kind of a calendar that would show dots corresponding to the amount of toDos and dots for each date. I have decided to skip this for now, as I still have a long way to go before I finish the course. I will come back to this eventually... I promise!!
+
+// let groupedDates = {};
+// //Dummy todo object constructor
+// const Todo = (_date, _name) => {
+//   const date = _date;
+//   const name = _name;
+//   return { date, name };
+// };
+
+// //Create some dummy todo objects
+// const t1 = Todo('01/01/2015', 'T1');
+// const t2 = Todo('01/01/2015', 'T2');
+// const t3 = Todo('01/02/2015', 'T3');
+// const t4 = Todo('01/03/2015', 'T4');
+// const t5 = Todo('01/03/2015', 'T5');
+// const t6 = Todo('01/04/2015', 'T6');
+
+// //Important bit
+// function generateTodoCluster(todoArray) {
+
+//   [...todoArray].forEach((todoItem) => {
+//     //If groupedDates doesn't have a key with this date, create one
+//     if (groupedDates[todoItem.date] == undefined) {
+//       groupedDates[todoItem.date] = [];
+//     }
+//     //Push this todo item to the key of this items date
+//     groupedDates[todoItem.date].push(todoItem);
+//   });
+// }
+
+// // Some logging and the results
+
+// generateTodoCluster([t1, t2, t3, t4, t5, t6]);
+
+// console.log(groupedDates);
+// /*
+//   groupedDates: {
+//     '01/01/2015': [ [Object], [Object] ],
+//     '01/02/2015': [ [Object] ],
+//     '01/03/2015': [ [Object], [Object] ],
+//     '01/04/2015': [ [Object] ]
+//   }
+
+// Clearer version of the above
+//   groupedDates: {
+//     '01/01/2015': [ { date: '01/01/2015', name: 'T1' }, { date: '01/01/2015', name: 'T2' } ],
+//     '01/02/2015': [ { date: '01/02/2015', name: 'T3' } ],
+//     '01/03/2015': [ { date: '01/03/2015', name: 'T4' }, { date: '01/03/2015', name: 'T5' } ],
+//     '01/04/2015': [ { date: '01/04/2015', name: 'T6' } ]
+//   }
+// */
 
 
-            // } else if (element1.name === element.date){
-            //     const dateArray = {
-            //         constructor() {
-            //             this.name = element.date;
-            //             this.array = [];
-            //         }
-            //     };
-            //     console.log(newDate.name); 
-            //     // create a date array that will hold objects related to that date
-            //     // to display dates and priority dots - no description
-            //     const dateToDo = {
-            //         date: element.date,
-            //         priority: element.priority,
-            //     };
-            //     dates.element1.array.push(dateToDo);
-            // }
-
-    //     if (!dates.includes(element.date)) {
-    //         console.log(element.date);
-    //         const dateArray = {
-    //             constructor() {
-    //                 this.
-    //             }
-    //         };
-    //         // create a date array that will hold objects related to that date
-    //         dates.push(dateArray);
-    //         // to display dates and priority dots - no description
-    //         const dateToDo = {
-    //             date: element.date,
-    //             priority: element.priority,
-    //         };
-    //         dateArray.push(dateToDo);
-    //         console.log(dates);
-    //     } else if (dates.includes(element.date)) {
-    //         let index = dates.findIndex(date => date.date === element.date);
-    //         const dateToDo = {
-    //             date: element.date,
-    //             priority: element.priority,
-    //         };
-    //         dateArray[index].push(dateToDo);
-    //         console.log(dateArray);
-    //     }
-    // });
-  
-
-// This function generates only one array - fix!!
-
-// generacja nowego obiektu/array z datami? wtedy możnaby dodawać toDoObjects do daty, data byłaby generowana codziennie wraz z arrayem, do którego można wciskać wydarzenia
-
-// a moze niech to dziaja z niszego poziomu? toDoObject?
+// for (let todoArray in groupedDates) {
+//   console.log(groupedDates[todoArray]); // { date: '01/01/2015', name: 'T1' },{ date: '01/01/2015', name: 'T2' }
+// }
