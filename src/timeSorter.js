fc5bde150b3b6a1bbe8a8cd7b0e12f5b21da793f
Krzysztof <ki.zwolak@gmail.com>
2022-11-30 02:41:56 +0100

    Change timeSorter logic

--- src/timeSorter.js
+++ src/timeSorter.js
@@ -1,29 +1,79 @@
 export default function timeSorter(object) {
-    const dates = [];
+
+    const dates = ['',];
+
     object.arrayOfToDos.forEach(element => {
-        if (!dates.includes(element.date)) {
-            const dateArray = [];
-            // create a date array that will hold objects related to that date
-            dates.push(dateArray);
-            // to display dates and priority dots - no description
-            const dateToDo = {
-                date: element.date,
-                priority: element.priority,
-            };
-            dateArray.push(dateToDo);
-            console.log(dates);
-        } else if (dates.includes(element.date)) {
-            let index = dates.findIndex(date => date.date === element.date);
-            const dateToDo = {
-                date: element.date,
-                priority: element.priority,
-            };
-            dateArray[index].push(dateToDo);
-            console.log(dateArray);
-        }
-    });
-    return dates;
-}; 
+        dates.forEach(date => {
+            if ((!(date.name == element.date)) || dates.length == 0) {
+                let newArrayName = element.date;
+                console.log(newArrayName);
+                console.log(element.date);
+                class dateArray {
+                    constructor(name) {
+                        this.name = name;
+                        this.array = [];
+                    }
+                };
+                const newDate = new dateArray(newArrayName);
+                console.log(newDate.name); 
+                const dateToDo = {
+                    date: element.date,
+                    priority: element.priority,
+                };
+                newDate.array.push(dateToDo);
+                dates.push(newDate);
+                break;
+            }
+            // } else if (element1.name === element.date){
+            //     const dateArray = {
+            //         constructor() {
+            //             this.name = element.date;
+            //             this.array = [];
+            //         }
+            //     };
+            //     console.log(newDate.name); 
+            //     // create a date array that will hold objects related to that date
+            //     // to display dates and priority dots - no description
+            //     const dateToDo = {
+            //         date: element.date,
+            //         priority: element.priority,
+            //     };
+            //     dates.element1.array.push(dateToDo);
+            // }
+    }
+    )
+}
+    ) 
+console.log(dates);
+return dates;
+}
+    //     if (!dates.includes(element.date)) {
+    //         console.log(element.date);
+    //         const dateArray = {
+    //             constructor() {
+    //                 this.
+    //             }
+    //         };
+    //         // create a date array that will hold objects related to that date
+    //         dates.push(dateArray);
+    //         // to display dates and priority dots - no description
+    //         const dateToDo = {
+    //             date: element.date,
+    //             priority: element.priority,
+    //         };
+    //         dateArray.push(dateToDo);
+    //         console.log(dates);
+    //     } else if (dates.includes(element.date)) {
+    //         let index = dates.findIndex(date => date.date === element.date);
+    //         const dateToDo = {
+    //             date: element.date,
+    //             priority: element.priority,
+    //         };
+    //         dateArray[index].push(dateToDo);
+    //         console.log(dateArray);
+    //     }
+    // });
+  
 
 // This function generates only one array - fix!!
 
