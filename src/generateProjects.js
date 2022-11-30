7ccec436ed917c97a0564dd3ae8c8c4c069b1e62
Krzysztof <ki.zwolak@gmail.com>
2022-11-30 19:44:27 +0100

    Main project-adding logic completed

--- src/generateProjects.js
+++ src/generateProjects.js
@@ -2,7 +2,23 @@ export default function generateProjects(project) {
     let projectsDiv = document.querySelector(".projects");
     let projectContainer = document.createElement('div');
     let projectTitle = document.createElement('h2');
+    
     projectTitle.textContent = project.title;
     projectContainer.appendChild(projectTitle);
-    projectsDiv.appendChild(projectContainer);  
+    projectsDiv.appendChild(projectContainer);
+    
+    let projectButtons = document.createElement('div');
+    projectContainer.appendChild(projectButtons);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+
+    let editButton = document.createElement('button');
+    editButton.textContent = "Edit";
+
+    projectButtons.appendChild(deleteButton);
+    projectButtons.appendChild(editButton);
+    projectContainer.appendChild(projectButtons);
+
+
 }
