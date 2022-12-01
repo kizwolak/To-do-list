68cbcce0f4f674076ff0627b14d4e8fae1af3bb8
Krzysztof <ki.zwolak@gmail.com>
2022-12-01 18:23:32 +0100

    Display new projects

--- src/formProcessor.js
+++ src/formProcessor.js
@@ -1,7 +1,8 @@
 import createProject from "./createProject";
-
+import generateProjects from "./generateProjects";
 
 export default function createNewItem(e) {
+    const modal = document.querySelector("#addNewModal");
     e.preventDefault();
     const title = document.querySelector("#title").value;
     const description = document.querySelector("#description").value;
@@ -9,6 +10,8 @@ export default function createNewItem(e) {
     const priority = document.querySelector('input[name = "prio"]:checked').value;
 
     console.log(title + " " + description + " " + date + " " + priority);
-    console.log(createProject(title, description, date, priority));
-    return createProject(title, description, date, priority)
+    const newProject = createProject(title, description, date, priority);
+    modal.style.display = "none";
+    generateProjects(newProject);
+    return newProject;
 }
